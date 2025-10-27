65674c19891cfe308b268825b56e417b8cf0d9ac
safaelgharras <ehsafaa7@gmail.com>
2025-10-27 12:45:16 +0000

    file

---
+++ Chapitre 2 – Appels AJAX avec fetch traitement JSON/script.js
@@ -0,0 +1,9 @@
+
+fetch("https://api.instagram.com/users/founticode")
+  .then(response => response.json())
+  .then(data => {
+    console.log("Données reçues :", data);
+  })
+  .catch(error => {
+    console.error("Erreur lors de la récupération :", error);
+  });
