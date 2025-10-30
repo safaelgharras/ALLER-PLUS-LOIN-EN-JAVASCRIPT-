db89ff8c989d17fd1c172467c4bcb407146ba568
safaelgharras <ehsafaa7@gmail.com>
2025-10-30 09:28:32 +0000

    file

--- Chapitre 4 – Affichage conditionnel et logique d’affichage/script.js
+++ Chapitre 4 – Affichage conditionnel et logique d’affichage/script.js
@@ -1,4 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
 let produits = [
   { nom: "Too Faced : Born This Way Concealer", prix: 550, catégorie: "makeup"},
   { nom: "Yves Saint Laurent : Rouge Pur Couture Lipstick", prix: 1157, catégorie: "makeup"},
@@ -8,40 +7,14 @@ let produits = [
 
 let liste = document.getElementById("produits");
 
-let filtresDiv = document.createElement("div");
-  filtresDiv.style = "text-align:center; margin:20px 0;";
+let produitsFiltres = produits.filter(p => p.prix < 100);
 
-  let select = document.createElement("select");
-  select.innerHTML = `
-    <option value="toutes">Toutes catégories</option>
-    <option value="makeup">Makeup</option>
-    <option value="perfum">Parfum</option>
-  `;
-
-  filtresDiv.appendChild(select);
- 
-  document.body.insertBefore(filtresDiv, liste);
-
-  function filtrer() {
-    let cat = select.value;
-    let filtres = produits.filter(p => p.prix >100 && (cat === "toutes" || p.catégorie === cat));
-
-    liste.innerHTML = "";
-    if (filtres.length === 0) {
-      liste.innerHTML = "<li>Aucun produit en promotion.</li>";
-    } else {
-      filtres.forEach(p => {
-        let li = document.createElement("li");
-        li.textContent = `${p.nom} – ${p.prix} DH`;
-        liste.appendChild(li);
-      });
-    }
-  }
-select.addEventListener("change", filtrer);
-  btn.addEventListener("click", () => {
-    select.value = "toutes";
-    filtrer();
+if (produitsFiltres.length === 0) {
+  liste.innerHTML = "<li>Aucun produit en promotion.</li>";
+} else {
+  produitsFiltres.forEach(p => {
+    let li = document.createElement("li");
+    li.textContent = `${p.nom} – ${p.prix} €`;
+    liste.appendChild(li);
   });
-
-  filtrer(); 
-});
+}
